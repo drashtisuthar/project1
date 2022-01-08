a10553b5d7244a93b5ff50422303ed8454f1cbe6
drashtisuthar <96520691+drashtisuthar@users.noreply.github.com>
2022-01-08 19:17:55 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,18 @@
+'''a=int(input("enter no:"))
+p=1
+for i in range(a):
+    p=p*(i+1)
+print(p)'''
+
+no=int(input("enter no:"))
+a=1
+i=1
+while i<=no:
+    a=a*i
+    i+=1
+print(a)
+
+'''import math
+print(math.factorial(6))'''
+
+
